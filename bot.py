b3904d183487922fe1fc6d0a2e1dd33937b9585e
John-Embate <72902619+John-Embate@users.noreply.github.com>
2024-05-22 12:27:52 +0800

    Added Giza Libraries.py
    Configured to add Giza Models, Giza Actions, Numpy, and Pandas

--- bot.py
+++ bot.py
@@ -22,6 +22,18 @@ from dotenv import load_dotenv
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
+#For Data Handling and DL Modeling
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+
+#For GizaTech Libraries and On chain proof of inference
+from giza_actions.model import GizaModel
+from giza_actions.action import Action, action
+from giza_actions.task import task
+
+
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -53,7 +65,52 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
 
+single_test_sample = None
+
+@task(name='Generate Prediction')
+def predict_output(data_for_prediction, model_id, version_id):
+    # Initialize the prediction model with the given model and version IDs.
+    prediction_model = GizaModel(id=model_id, version=version_id)
+    
+    # Perform prediction using the model's predict method and ensure output integrity.
+    prediction_result, prediction_tracking_id = prediction_model.predict(
+        input_feed={"model_input": data_for_prediction}, 
+        verifiable=True,
+        job_size="XL",
+    )
+    print(prediction_result)
+    print(prediction_tracking_id)
+    return prediction_result, prediction_tracking_id
+
+@action(name='Execute Prediction Process Supply', log_prints=True)
+def execute_prediction_supply():
+    model_identifier = 368  # Customize with the specific model ID for Supply Rate Model
+    version_identifier = 11  # Customize with the specific version ID for Supply Rate Model
+    
+    # Using a single sample from the test set for making a prediction
+    prediction_result, operation_tracking_id = predict_output(single_test_sample, model_identifier, version_identifier) 
+    return prediction_result, operation_tracking_id
+
+@action(name='Execute Prediction Process Borrow', log_prints=True)
+def execute_prediction_borrow():
+    model_identifier = 619  # Customize with the specific model ID for Supply Rate Model
+    version_identifier = 1  # Customize with the specific version ID for Supply Rate Model
+    
+    # Using a single sample from the test set for making a prediction
+    prediction_result, operation_tracking_id = predict_output(single_test_sample, model_identifier, version_identifier) 
+    return prediction_result, operation_tracking_id
+
 def main() -> None:
+
+    # Load the CSV files for smaple predictions
+    X_df = pd.read_csv('X_tensor_Compound_V2.csv')
+    y_df = pd.read_csv('y_tensor_Compound_V2.csv')
+    
+    # Convert DataFrames to tensors
+    X_tensor = torch.tensor(X_df.values, dtype=torch.float32)
+    y_tensor = torch.tensor(y_df.values.squeeze(), dtype=torch.float32)  # Assuming y is 1D
+    
+    
     """Start the bot."""
     load_dotenv()
 
